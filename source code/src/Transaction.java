4602cba5a653efcf9840eb29d0f13ef5c882ba3e
habiba-menshawy <habibamenshawy060@gmail.com>
2022-12-01 14:08:38 +0200

    Update Transaction.java

--- source code/src/Transaction.java
+++ source code/src/Transaction.java
@@ -1,2 +1,34 @@
 public class Transaction {
+    Services service;
+    Customer customer;
+    double  pay_amount;
+    boolean refund=false;
+    int trans_ID;
+    public Transaction (Services s, Customer c,  double a,int id)
+    {
+        this.service=s;
+        this.customer=c;
+        this.pay_amount=a;
+        this.trans_ID=id;
+    }
+    public void setRefund(boolean r)
+    {
+        this.refund=r;
+    }
+    public Services getService()
+    {
+        return this.service;
+    }
+    public Customer getCustomer()
+    {
+        return this.customer;
+    }
+    public double getPay_amount()
+    {
+        return this.pay_amount;
+    }
+    public boolean getRefund()
+    {
+        return this.refund;
+    }
 }
