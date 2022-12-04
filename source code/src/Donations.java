e730cc9dfe52387474d51ab1f7cfa137cc7514c9
habibayasser <118079766+Habiba554@users.noreply.github.com>
2022-12-04 22:55:06 +0200

    Services Updates

--- source code/src/Donations.java
+++ source code/src/Donations.java
@@ -1,11 +1,23 @@
-public class Donations implements Services{
+import java.util.Vector;
+
+public class Donations extends Services{
     public static double discount = 0.0;
+    private String location;
+    String name="Donations services";
+
     public void updateDiscount(double amount) {
         discount = discount + amount;
     }
 
-    @Override
     public double getDiscount() {
         return discount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 }
